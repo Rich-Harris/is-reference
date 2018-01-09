9e62ba9ddae5fdf7b3298039c8de3d7d439fa8a6
Mattias Buelens <mattias@buelens.com>
2018-01-09 23:06:52 +0100

    Separate source files from output files
    * Move module.js to src/index.js
    * Generate UMD and ES bundles in dist
    * Update Rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,11 @@
 export default {
-	entry: 'module.js',
-	dest: 'index.js',
-	format: 'umd',
-	moduleName: 'isReference'
+	input: 'src/index.js',
+	output: [{
+		file: 'dist/is-reference.js',
+		format: 'umd',
+		name: 'isReference'
+	}, {
+		file: 'dist/is-reference.es.js',
+		format: 'es'
+	}]
 };
