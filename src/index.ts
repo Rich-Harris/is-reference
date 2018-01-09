b1dfc7fc998fa76ec32899010b8b4e638e5c8818
Mattias Buelens <mattias@buelens.com>
2018-01-09 23:27:43 +0100

    Fix type errors

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Node } from 'estree';
+import { MemberExpression, Node } from 'estree';
 
 export default function isReference (node: Node, parent: Node): boolean {
 	if (node.type === 'MemberExpression') {
@@ -13,14 +13,15 @@ export default function isReference (node: Node, parent: Node): boolean {
 
 		// TODO is this right?
 		if (parent.type === 'MemberExpression' || parent.type === 'MethodDefinition') {
-			return parent.computed || node === parent.object;
+			return parent.computed || node === (<MemberExpression>parent).object;
 		}
 
 		// disregard the `bar` in `{ bar: foo }`, but keep it in `{ [bar]: foo }`
 		if (parent.type === 'Property') return parent.computed || node === parent.value;
 
 		// disregard the `bar` in `class Foo { bar () {...} }`
-		if (parent.type === 'MethodDefinition') return false;
+		// TODO Dead code? Already handled above
+		// if (parent.type === 'MethodDefinition') return false;
 
 		// disregard the `bar` in `export { foo as bar }`
 		if (parent.type === 'ExportSpecifier' && node !== parent.local) return false;
